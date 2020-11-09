024c1b572f89603c3bf11d2b82a9fe120c0e29b9
tjelle <tjelle97@gmail.com>
2020-11-09 11:54:21 +0100

    working

--- app.py
+++ app.py
@@ -18,3 +18,8 @@ def elements():
 @app.route('/generic.html')
 def generic():
     return render_template('pages/generic.html')
+
+
+@app.route("/index.html")
+def index():
+    return render_template("pages/index.html")
